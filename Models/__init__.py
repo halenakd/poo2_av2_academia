e21e56db83e5f0cb8adaa489c24e7e4552f464d7
halenakd <98594021+halenakd@users.noreply.github.com>
2022-07-04 15:50:04 -0300

    Delete Models directory

--- Models/__init__.py
+++
@@ -1,3 +0,0 @@
-__all__ = ['endereco', 'pessoa', 'usuario', 'colaborador', 'professor',
-    'exercicio', 'treino', 'exercicioTreino', 'aluno', 'alimento', 'dieta',
-    'dietaAlimento', 'dietaAluno', 'avaliacaoFisica']
